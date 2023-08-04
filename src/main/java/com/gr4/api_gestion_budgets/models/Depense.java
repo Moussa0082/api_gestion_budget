aa37f4c75d55475e17bd8237b72050b89fd2c387
Almaimoune Mohamed Elmoctar <elmo.almaimoune@odc.local>
2023-08-04 12:16:47 +0000

    Création de la classe Categorie et depense

---
+++ src/main/java/com/gr4/api_gestion_budgets/models/Depense.java
@@ -0,0 +1,45 @@
+package com.gr4.api_gestion_budgets.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Data
+public class Depense {
+
+    public enum TypeDepense{
+        MENSUEL,
+        QUOTIDIEN,
+        HEBDOMADAIRE
+    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Enumerated(EnumType.STRING)
+    @Column
+    private TypeDepense typeDepense;
+
+    @Column
+    private Integer mont_depense;
+
+    @Temporal(TemporalType.DATE)
+    @Column
+    private Date date_depense;
+
+    @Column
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "Category")
+    private Categorie categorie;
+
+    @ManyToOne
+    @JoinColumn(name = "id_utilisateur")
+    private Utilisateur utilisateur;
+
+
+
+}
