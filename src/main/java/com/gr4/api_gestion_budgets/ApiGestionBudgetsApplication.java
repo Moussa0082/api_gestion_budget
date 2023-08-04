7f521f62a418e5775a1204f041577cf24c2fc598
unknown <bane8252@gmail.com>
2023-08-04 12:08:07 +0000

    one

---
+++ src/main/java/com/gr4/api_gestion_budgets/ApiGestionBudgetsApplication.java
@@ -0,0 +1,13 @@
+package com.gr4.api_gestion_budgets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGestionBudgetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGestionBudgetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gr4/api_gestion_budgets/ApiGestionBudgetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gr4.api_gestion_budgets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGestionBudgetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
