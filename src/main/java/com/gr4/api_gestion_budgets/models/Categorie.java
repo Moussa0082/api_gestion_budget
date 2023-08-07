c20c0ffcf27d8ed55ac7686d5c4c31f34109a0ef
Almaimoune9 <elmo.almaimoune@odc.local>
2023-08-07 16:41:21 +0000

    modifications

--- src/main/java/com/gr4/api_gestion_budgets/models/Categorie.java
+++ src/main/java/com/gr4/api_gestion_budgets/models/Categorie.java
@@ -1,7 +1,6 @@
 package com.gr4.api_gestion_budgets.models;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
 
