6b53ab962f5626b8b55be4e8bdb1f7c9d57c7f60
kante.fode <fodekanteodc@gmail.com>
2023-08-08 15:19:43 +0000

    alerte modification

--- src/main/java/com/gr4/api_gestion_budgets/service/AlerteServiceImp.java
+++ src/main/java/com/gr4/api_gestion_budgets/service/AlerteServiceImp.java
@@ -3,11 +3,14 @@ package com.gr4.api_gestion_budgets.service;
 import com.gr4.api_gestion_budgets.repository.AlerteRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
 
 @Service
 @AllArgsConstructor
-
 public class AlerteServiceImp implements AlerteService {
     private final AlerteRepository alerteRepository;
     @Override
@@ -15,6 +18,16 @@ public class AlerteServiceImp implements AlerteService {
     public AlerteRepository message(String message){
         return alerteRepository;
     }
+        @Autowired
+        private JavaMailSender mailSender;
 
+        public void sendEmail(String to, String subject, String body) {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(body);
 
-}
+            mailSender.send(message);
+        }
+    }
+
